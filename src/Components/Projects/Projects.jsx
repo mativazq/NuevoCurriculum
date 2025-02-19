ca26af2d6e450288b3d050b31f043d598ff91ef3
Mati Vazquez <mativazquezfotografia@hotmail.com>
2025-02-18 23:55:36 -0300

    end projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,45 +1,101 @@
 import React from "react";
 import "./projects.css";
-import projectImage from "../../Images/Tiendita.png"; // Asegúrate de que la imagen esté en la carpeta correcta
-
+import projectImage from "../../Images/Tiendita.png";
+import ImgPython from "../../Images/PythonDjango.png";
+import ImgGefes from "../../Images/Gefes.png";
 
 const Projects = () => {
   return (
     <section className="projects-section">
       <h2 className="projects-title">Proyectos</h2>
 
+{/* Proyecto React */}
       <div className="project-card">
-        {/* Imagen del proyecto */}
+
         <img src={projectImage} alt="Proyecto" className="project-image" />
 
-        {/* Contenido del proyecto */}
         <div className="project-content">
-          <h3 className="project-name">Kawaii Universe</h3>
+          <h3 className="project-name">Mi Tiendita NBA</h3>
 
-          {/* Tecnologías usadas */}
+
           <div className="tech-stack">
-            <span className="tech-tag">HTML</span>
+            <span className="tech-tag">React</span>
+            <span className="tech-tag">Vite</span>
             <span className="tech-tag">SASS</span>
-            <span className="tech-tag">JavaScript</span>
+            <span className="tech-tag">Firebase</span>
             <span className="tech-tag">Bootstrap</span>
           </div>
 
-          {/* Descripción */}
+
           <p className="project-description">
-            Proyecto realizado de forma freelance, con un equipo de diseño y un programador más. Construimos esta web de presentación adaptable a distintos dispositivos.
+          Proyecto realizado de forma personal, se trata de un ecommerce con catalogo de productos y subida de estos al carrito, cuenta con orden de compra a la hora de efectuar esta misma, la cual se envía al e-mail del vendedor para preparar este pedido al igual que el formulario de contacto.
           </p>
 
-          {/* Botones */}
+
           <div className="buttons-container">
-            <button className="button">
-              <p>Deploy</p>
-            </button>
-            <button className="button">
-            <p>Code</p>
-            </button>
+            <a href="https://github.com/mativazq/NuevoCurriculum" className="button">Code</a>
+            <a href="https://matias-vazquez.netlify.app/" className="button">Deploy</a>
           </div>
         </div>
       </div>
+
+{/* Proyecto Python */}
+      <div className="project-card">
+
+        <img src={ImgPython} alt="Proyecto" className="project-image" />
+
+
+        <div className="project-content">
+          <h3 className="project-name">Bookings</h3>
+
+
+          <div className="tech-stack">
+            <span className="tech-tag">Python</span>
+            <span className="tech-tag">Django</span>
+            <span className="tech-tag">SQLite</span>
+          </div>
+
+
+          <p className="project-description">
+          Proyecto realizado de forma personal academicamente. Este proyecto es una aplicación web de reservas creada con Django. Permite a los usuarios registrarse, crear salas y hacer reservas.
+          </p>
+
+
+          <div className="buttons-container">
+            <a href="https://github.com/mativazq/trabajo_final_vazquez" className="button">Code</a>
+            <a href="https://github.com/mativazq/trabajo_final_vazquez" className="button">Deploy</a>
+          </div>
+        </div>
+      </div>
+
+      {/* Maquetado HTML - CSS */}
+      <div className="project-card">
+
+<img src={ImgGefes} alt="Proyecto" className="project-image" />
+
+
+<div className="project-content">
+  <h3 className="project-name">Gefes con G</h3>
+
+
+  <div className="tech-stack">
+    <span className="tech-tag">HTML</span>
+    <span className="tech-tag">CSS</span>
+    <span className="tech-tag">SASS</span>
+  </div>
+
+
+  <p className="project-description">
+  Maquetado realizado de forma personal, se trata de una landing page informativa junto con dos pestañas aparte de cursos con carrousel y de contacto.
+  </p>
+
+
+  <div className="buttons-container">
+    <a href="https://github.com/mativazq/GefesConG" className="button">Code</a>
+    <a href="https://mativazq.github.io/GefesConG//" className="button">Deploy</a>
+  </div>
+</div>
+</div>
     </section>
   );
 };
